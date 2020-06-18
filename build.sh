2f05c95f465418f81a6fe4ce178490ecc37ebfea
Vaibhav Dhoke <vaibhavdhoke1@gmail.com>
2020-06-18 00:07:58 -0400

    Network Infrastructure for A5 Terraform

--- build.sh
+++ build.sh
@@ -6,16 +6,26 @@ terraform plan -var="vpccider=10.0.0.0/24" \
                -var="subnet2cider=10.0.0.64/26" \
                -var="subnet3cider=10.0.0.128/25" \
                -var="routedestcidr=0.0.0.0/0" \
+               -var="region=us-east-1" \
                -var="availablezone1=us-east-1a" \
                -var="availablezone2=us-east-1b" \
                -var="availablezone3=us-east-1c" \
-               -var="vpctag=a4_vpc_csye6225"   
+               -var="vpctag=a4_vpc_csye6225" \
+               -var="ami_id=ami-05e623ee887a22b09" \
+               -var="rds_username=csye6225_su2020" \
+               -var="rds_password=vrd141293" \
+               -var="db_name=csye6225" 
 terraform apply -auto-approve -var="vpccider=10.0.0.0/24" \
                               -var="subnet1cider=10.0.0.0/26" \
                               -var="subnet2cider=10.0.0.64/26" \
                               -var="subnet3cider=10.0.0.128/25" \
                               -var="routedestcidr=0.0.0.0/0" \
+                              -var="region=us-east-1" \
                               -var="availablezone1=us-east-1a" \
                               -var="availablezone2=us-east-1b" \
                               -var="availablezone3=us-east-1c" \
-                              -var="vpctag=a4_vpc_csye6225" 
+                              -var="vpctag=a4_vpc_csye6225" \
+                              -var="ami_id=ami-05e623ee887a22b09" \
+                              -var="rds_username=csye6225_su2020" \
+                              -var="rds_password=vrd141293" \
+                              -var="db_name=csye6225" 
--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-provider "aws" {
-  region = "us-east-1"
+variable "region" {
+  type = "string"
 }
 
 variable "vpccider" {
@@ -29,6 +29,11 @@ variable "availablezone3" {
 variable "vpctag" {
   type = "string"
 }
+
+provider "aws" {
+  region = "${var.region}"
+}
+
 resource "aws_vpc" "a4_vpc_csye6225" {
   cidr_block = "${var.vpccider}"
   enable_dns_hostnames = true
@@ -95,4 +100,208 @@ resource "aws_route_table_association" "association2" {
 resource "aws_route_table_association" "association3" {
   subnet_id      = "${aws_subnet.subnet3.id}"
   route_table_id = "${aws_route_table.route_table.id}"
+}
+
+
+resource "aws_security_group" "application" {
+  name        = "application"
+  description = "Security group for Book Web Application"
+  vpc_id      = "${aws_vpc.a4_vpc_csye6225.id}"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_security_group" "database" {
+  name        = "database"
+  description = "Security group for RDS instance in AWS"
+  vpc_id      = "${aws_vpc.a4_vpc_csye6225.id}"
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    cidr_blocks     = [aws_vpc.a4_vpc_csye6225.cidr_block]
+    security_groups = ["${aws_security_group.application.id}"]
+  }
+}
+resource "aws_kms_key" "kmskey" {
+  description             = "KMS key for encryption of s3 bucket objects"
+  deletion_window_in_days = 7
+}
+resource "aws_s3_bucket" "bucket" {
+  bucket = "webapp.vaibhav.dhoke"
+  acl    = "private"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "${aws_kms_key.kmskey.arn}"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+  lifecycle_rule {
+    id      = "move"
+    enabled = true
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+  }
+}
+resource "aws_db_subnet_group" "databasesubnet" {
+  name       = "databasesubnetgroup"
+  subnet_ids = ["${aws_subnet.subnet1.id}", "${aws_subnet.subnet2.id}", "${aws_subnet.subnet3.id}"]
+}
+
+variable "rds_username" {
+  type = "string"
+}
+variable "rds_password" {
+  type = "string"
+}
+variable "db_name" {
+  type = "string"
+}
+
+
+resource "aws_db_instance" "csye6225-su2020" {
+  allocated_storage      = 20
+  engine                 = "mysql"
+  instance_class         = "db.t3.micro"
+  identifier             = "csye6225-su2020"
+  username               = "${var.rds_username}"
+  password               = "${var.rds_password}"
+  publicly_accessible    = "false"
+  name                   = "${var.db_name}"
+  multi_az               = "false"
+  vpc_security_group_ids = ["${aws_security_group.database.id}"]
+  db_subnet_group_name   = "${aws_db_subnet_group.databasesubnet.name}"
+}
+
+variable "ami_id" {
+  type = "string"
+}
+resource "aws_instance" "web" {
+  ami                     = "${var.ami_id}"
+  instance_type           = "t2.micro"
+  disable_api_termination = "false"
+  subnet_id               = "${aws_subnet.subnet1.id}"
+  vpc_security_group_ids  = ["${aws_security_group.application.id}"]
+  depends_on              = [aws_db_instance.csye6225-su2020]
+  key_name                = "csye6225_su20_aws"
+  root_block_device {
+    volume_type           = "gp2"
+    volume_size           = 20
+    delete_on_termination = "true"
+  }
+  tags = {
+    Name = "EC2 Instance from Terraform"
+  }
+  user_data = <<-EOFS
+#!/bin/bash
+sudo mkdir /home/ubuntu/webapp
+sudo chmod 777 /home/ubuntu/webapp
+sudo mkdir /home/ubuntu/webapp/config
+cat > /home/ubuntu/webapp/config/config.json << EOF
+{
+"development": {
+    "username": "${aws_db_instance.csye6225-su2020.username}",
+    "password": "${aws_db_instance.csye6225-su2020.password}",
+    "database": "${aws_db_instance.csye6225-su2020.name}",
+    "host": "${aws_db_instance.csye6225-su2020.address}",
+    "dialect": "mysql",
+    "operatorsAliases": false,
+    "s3bucket": "${aws_s3_bucket.bucket.bucket}",
+    "region": "${var.region}"
+  }
+}
+EOF
+EOFS
+}
+
+resource "aws_dynamodb_table" "dynamodb" {
+  name           = "csye6225"
+  hash_key       = "id"
+  attribute {
+    name = "id"
+    type = "S"
+  }
+  read_capacity  = 20
+  write_capacity = 20
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = "WebAppS3"
+  description = "EC2 S3 policy"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:DeleteObject",
+        "s3:PutObject",
+        "s3:GetObject"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+                "arn:aws:s3:::webapp.vaibhav.dhoke",
+                "arn:aws:s3:::webapp.vaibhav.dhoke/*"
+            ]
+    }
+  ]
+}
+EOF
+}
+
+
+resource "aws_iam_role" "role" {
+  name = "EC2-CSYE6225"
+  assume_role_policy = <<EOF
+{
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Action": "sts:AssumeRole",
+          "Principal": {
+            "Service": "ec2.amazonaws.com"
+          },
+          "Effect": "Allow",
+          "Sid": ""
+        }
+      ]
+}
+EOF
+}
+
+
+resource "aws_iam_role_policy_attachment" "rolepolicyattachment" {
+  role       = "${aws_iam_role.role.name}"
+  policy_arn = "${aws_iam_policy.policy.arn}"
 }
