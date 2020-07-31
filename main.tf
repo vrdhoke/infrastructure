da19ffc6b5b898bc65b5926fd1752998842e396a
Vaibhav Dhoke <vaibhavdhoke1@gmail.com>
2020-07-31 16:27:00 -0400

    Enabling security group for SSH

--- main.tf
+++ main.tf
@@ -115,6 +115,12 @@ resource "aws_security_group" "application" {
     security_groups    = ["${aws_security_group.lb_securitygroup.id}"]
   }
   ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
     from_port   = 5000
     to_port     = 5000
     protocol    = "tcp"
